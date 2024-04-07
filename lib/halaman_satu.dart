5163649b5056d738e8df848dba1edd5afecbb624
Sendhy Rhysa <sbillah48@gmail.com>
2024-04-07 09:16:33 +0800

    Navigasi_WidgetHero

---
+++ lib/halaman_satu.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:navigasi_widgethero/halaman_dua.dart';
+
+class SourcePage extends StatelessWidget {
+  const SourcePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Halaman Asal')
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => const DestinationPage()),
+          );
+        },
+        child: const Hero(
+          tag: 'avatarTag',
+          child: CircleAvatar(
+            backgroundImage:AssetImage("images/avatar.jpg"),
+            radius: 50,
+            ),
+        ),
+      ),
+    );
+  }
+}
